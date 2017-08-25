363ed2253a487dd7eb78030eda096f1fc6251937
Joe Kramer <joekramer1125@gmail.com>
2017-08-25 11:59:15 -0700

    add information to player detail page

--- src/app/player-detail/player-detail.component.ts
+++ src/app/player-detail/player-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Player } from '../player.model'; 
 
 @Component({
   selector: 'app-player-detail',
